f64661c815649225a7ad9c2d431d9fbd4816749f
daviswcci <79462946+daviswcci@users.noreply.github.com>
2022-09-19 10:22:14 -0400

    added cloning interface

---
+++ Fall2022 Car Workshop/Fall2022 Car Workshop/IDoubleCloneable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2022_Car_Workshop
+{
+    internal interface IDoubleCloneable
+    {
+    }
+}
